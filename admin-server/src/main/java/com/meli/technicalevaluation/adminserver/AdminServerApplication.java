4273692f01800835d8e728191f8962e420c7faaf
@seven <hfredy717@outlook.com>
2021-10-09 23:28:05 -0500

    Modificación para despliegue

--- admin-server/src/main/java/com/meli/technicalevaluation/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/meli/technicalevaluation/adminserver/AdminServerApplication.java
@@ -3,9 +3,9 @@ package com.meli.technicalevaluation.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class AdminServerApplication {
